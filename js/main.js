cf970299220888ce00b45cc3929e6f9c71bc5ee6
manohar <manumanohar1527@gmail.com>
2020-04-26 19:43:13 +0530

    updated script

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // 1) Personal details: First name: Should be only alphabets with min 4 and max 10 characters. Email-id: Should be a valid email id with @ symbol. Gender: Male, Female or other- Only 1 should be selected. Age: Should only be numbers between 20-23. DOB: Should match with age field value. Phone: Should be exactly 10 digits.
 
 // 2) Upload section: Resume upload option.
@@ -27,45 +26,57 @@ const ph = document.getElementById('phoneNo');
 const marks = document.getElementById('marks');
 const myInput = document.getElementById("psw");
 const myInput2 = document.getElementById("psw2");
+const email = document.getElementById('email')
+btn.addEventListener('click', validate);
 
-btn.addEventListener('click',validate);
-
-function validate(e){
+function validate(e) {
     e.preventDefault();
-    let x = name.value;  
-    if(x.isNaN){
-        alert("Should be only alphabets with min 4 and max 10 characters");
-        return false; 
+    let x = name.value;
+    if (name.value == "") {
+        alert("Enter the name")
     }
-    if (x.length<4 || x.length>10 ) {
+    if (x.isNaN) {
         alert("Should be only alphabets with min 4 and max 10 characters");
         return false;
-      }
+    }
+    if (x.length < 4 || x.length > 10) {
+        alert("Should be only alphabets with min 4 and max 10 characters");
+        return false;
+    }
+
+    //email
+    if (email.value == "") {
+        alert("Enter the email")
+    }
     //phone
     let phone = ph.value;
-    if(!phone.isNaN){
-       if(phone.length>10 ){
-           alert("invalid phone number");
-           return false;
-       }
+     if(ph.value==""){
+         alert("Enter the phone No")
+     }
+    if (!phone.isNaN) {
+        if (phone.length > 10) {
+            alert("invalid phone number");
+            return false;
+        }
     }
     //marks
     let mark = marks.value;
-    if(mark.length>3){
+    if (mark.length > 3) {
         alert("invalid marks");
         return false;
-        
+
     }
     //password
     // Validate length
-    if(myInput.value.length <= 8) {
-      alert("input strong password");
-    } 
-    if(!(myInput.value===myInput2.value)){
+    myInput2.style.border = 'none';
+    if (myInput.value.length <= 8) {
+        alert("input strong password");
+    }
+    if (!(myInput.value === myInput2.value)) {
         myInput2.style.border = '1px solid red';
-        myInput2.value="";
+        myInput2.value = "";
         alert("Enter the correct password");
         return false
     }
-  
+
 }
